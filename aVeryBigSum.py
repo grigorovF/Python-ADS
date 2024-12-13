468a2f685f583c79d5b2d5f13999620bc095eeb0
Filip Grigorov <filipgrigorov44@gmail.com>
2024-12-13 13:02:12 +0100

    aVeryBigSum

---
+++ aVeryBigSum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def aVeryBigSum(ar):
+    
+    for i in (0, len(ar)):
+        sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
