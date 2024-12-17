efd69ce10dc85058ddfab9ccf22d36cb62d948be
Filip Grigorov <filipgrigorov44@gmail.com>
2024-12-17 11:53:28 +0100

    Create reversedArray.py

---
+++ reversedArray.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def reverseArray(a):
+    reservedArray = [] 
+    reservedArray = a[::-1]
+    
+    return reservedArray
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
