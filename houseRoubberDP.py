3051d43e0a2f1385e1223e999d47758d8b65ffe6
Filip Grigorov <filipgrigorov44@gmail.com>
2025-01-17 15:33:49 +0100

    Update houseRoubberDP.py

--- houseRoubberDP.py
+++ houseRoubberDP.py
@@ -1,18 +1,16 @@
 def houseRobber(nums):
-    if not nums:
+    n = len(nums)
+    if n==0:
         return 0
-    if len(nums) == 1:
+    if n==1:
         return nums[0]
     
-    n = len(nums)
-    dp = [0] * n
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
-
-    for i in range(2, n):
-        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-
-    return dp[-1]
+    prev1, prev2 = 0, 0
+    for num in nums:
+        current = max(prev1, prev2+num)
+        prev2 = prev1
+        prev1 = current
+    return prev1
 
 if __name__ == '__main__':
     nums = list(map(int, input().split()))
